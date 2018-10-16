0104b4632f4861cdefd87de40c5041bd2cf3bd5c
Mandeep <>
2018-10-15 20:53:27 -0600

    Added more UI. Builds without error. Have add loop

--- Lab2Assignment/src/UI.java
+++ Lab2Assignment/src/UI.java
@@ -6,6 +6,9 @@
  * @author mDhillon
  *
  */
+
+import java.util.Scanner;
+
 public class UI {
 
 	/**
@@ -21,16 +24,45 @@ public class UI {
 		  System.out.println("Menu:\n");
 		  System.out.println("A - Add a node to the list\nR - Retrieve a node from the list");
 		  System.out.println("D - Delete a node from the list\nH - Print the linked list from head\n");
+		  System.out.print("Q - Quit the program\n\nEnter your option: ");
 		  
+		  Scanner in = new Scanner(System.in);
 		  
+		  char c = in.next().charAt(0);
 		  //le.addElement();
 		  
-		  ListElement le1 = new ListElement();
-		  le1.setData(2);
-		  
-		  le.addElement(le1);
-		  
-		  le.printLinkedListHead();
+		  if (c == 'A' || c == 'a') {
+			  System.out.print("Enter an integer for the new element: ");
+			  int i = in.nextInt();
+			  ListElement temp = new ListElement();
+			  temp.setData(i);
+			  le.addElement(temp);  
+		  } else if (c == 'R' || c == 'r') {
+			  System.out.print("Enter the index of the node you want to retrieve: ");
+			  int i = in.nextInt();
+			  le.getElement(i);
+		  } else if (c == 'D' || c == 'd') {
+			  System.out.print("Enter the index of the node you want to delete: ");
+			  int i = in.nextInt();
+			  le.deleteElement(i);
+		  } else if (c == 'H' || c == 'h') {
+			  le.printLinkedListHead();
+		  } else if (c == 'Q' || c =='q'){
+			  System.out.println("Quitting...");
+			  System.out.println("...............");
+			  System.out.println("...................");
+			  try {
+				Thread.sleep(30000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				in.close();
+				System.exit(0);
+			}
+		  } else {
+			  System.out.println("Invalid input!\nTry again!");
+		  }
 	}
 
 }
