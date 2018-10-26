f6205f8869468841a25d411815106939f5fa5694
Mandeep <>
2018-10-25 23:25:13 -0600

    Added while loop to go through options as many times as needed

--- Lab2Assignment/src/UI.java
+++ Lab2Assignment/src/UI.java
@@ -30,27 +30,36 @@ public class UI {
 		  
 		  char c = in.next().charAt(0);
 		  //le.addElement();
-		  
+		 do {
 		  if (c == 'A' || c == 'a') {
 			  System.out.print("Enter an integer for the new element: ");
 			  int i = in.nextInt();
 			  ListElement temp = new ListElement();
 			  temp.setData(i);
-			  le.addElement(temp);  
+			  le.addElement(temp);
+			  System.out.print("Enter next option: ");
+			  c = in.next().charAt(0);	  
 		  } else if (c == 'R' || c == 'r') {
 			  System.out.print("Enter the index of the node you want to retrieve: ");
 			  int i = in.nextInt();
 			  le.getElement(i);
+			  System.out.print("\nEnter next option: ");
+			  c = in.next().charAt(0);
 		  } else if (c == 'D' || c == 'd') {
 			  System.out.print("Enter the index of the node you want to delete: ");
 			  int i = in.nextInt();
 			  le.deleteElement(i);
+			  System.out.print("\nEnter next option: ");
+			  c = in.next().charAt(0);
 		  } else if (c == 'H' || c == 'h') {
 			  le.printLinkedListHead();
+			  System.out.print("\nEnter next option: ");
+			  c = in.next().charAt(0);
 		  } else if (c == 'Q' || c =='q'){
 			  System.out.println("Quitting...");
 			  System.out.println("...............");
 			  System.out.println("...................");
+			  System.out.println("Linked list has been unlinked!");
 			  try {
 				Thread.sleep(30000);
 			} catch (InterruptedException e) {
@@ -62,7 +71,9 @@ public class UI {
 			}
 		  } else {
 			  System.out.println("Invalid input!\nTry again!");
-		  }
+			  System.out.print("Enter your option: ");
+			  c = in.next().charAt(0);
+		  }}while(c != 'Q' || c != 'q');
 	}
 
 }
